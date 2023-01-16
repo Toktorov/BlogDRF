9b17c141949adddffb62381ee8c679161c6e3d5d
Toktorov <ktoktorov2005@gmail.com>
2023-01-16 17:19:46 +0600

    add user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    phone = models.CharField(
+        max_length=255,
+        verbose_name="Телефонный номер"
+    )
+    profile_image = models.ImageField(
+        upload_to="profile_images/",
+        verbose_name="Фотография профиля"
+    )
+
+    def __str__(self) -> str:
+        return self.username 
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
